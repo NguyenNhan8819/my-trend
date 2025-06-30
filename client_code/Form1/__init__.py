3cb38620117ce0daa56e0570b0a6d6a83edab8a0
nhannp@vinanhatrang.vn <nhannp@vinanhatrang.vn>
2025-06-30 04:39:03 +0000

    Edited ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,5 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-import anvil.server
 import plotly.graph_objects as go
 
 class Form1(Form1Template):
@@ -12,29 +11,49 @@ class Form1(Form1Template):
     self.data_points = []       # Lưu (phút, nhiệt độ)
     self.total_seconds = 0      # Đếm thời gian thực tế
     self.temp_box.text = 30
+    self.timer_1.interval = 0
 
-    self.timer_1.enabled = False  # Chưa bắt đầu
 
   def start_button_click(self, **event_args):
     """Khi nhấn nút Bắt đầu"""
-    self.timer_1.enabled = True
+    self.timer_1.interval = 5
     self.start_button.enabled = False  # Không cho nhấn lại
     self.status_label.text = "Đang ghi dữ liệu mỗi 5 giây..."
+
 
   def timer_1_tick(self, **event_args):
     """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
     """Chạy mỗi 5 giây"""
     try:
       temp = float(self.temp_box.text)
-      minute = self.total_seconds // 60
+      minute = self.total_seconds // 5
       self.data_points.append((minute, temp))
 
       self.total_seconds += 5
-      self.fig = anvil.server.call('update_plot')
-      # self.update_plot()
+      self.update_plot()
       self.status_label.text = f"Đã thêm {temp}°C tại phút {minute}"
 
     except ValueError:
       self.status_label.text = "Không ghi: nhiệt độ chưa hợp lệ"
-
-    self.plot_1.figure = self.fig
+  def update_plot(self):
+    if not self.data_points:
+      self.plot_1.figure = go.Figure()
+      return
+    times = [t[0] for t in self.data_points]
+    temps = [t[1] for t in self.data_points]
+    # fig = go.Figure()
+    data =  go.Scatter(
+      x=times,
+      y=temps,
+      mode='lines',
+      name="Nhiệt độ"
+    )
+    # fig.update_layout(
+    #   title="Trend nhiệt độ mỗi 5 giây",
+    #   xaxis_title="Thời gian (phút)",
+    #   yaxis_title="Nhiệt độ (°C)",
+    #   template="plotly_white"
+    # )
+    self.plot_1.data = data
+    print (self.data_points)
+    
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -13,23 +13,3 @@ import plotly.graph_objects as go
 #   print("Hello, " + name + "!")
 #   return 42
 #
-@anvil.server.callable
-def update_plot(self):
-  if not self.data_points:
-    self.plot_1.figure = go.Figure()
-    return
-  times = [t[0] for t in self.data_points]
-  temps = [t[1] for t in self.data_points]
-  fig = go.Figure()
-  fig.add_trace( go.Scatter(
-    x=times,
-    y=temps,
-    mode='lines+markers',
-    name="Nhiệt độ"
-  ))
-  fig.update_layout(
-    title="Trend nhiệt độ mỗi 5 giây",
-    xaxis_title="Thời gian (phút)",
-    yaxis_title="Nhiệt độ (°C)",
-    template="plotly_white"
-  )
