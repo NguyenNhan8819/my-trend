8c45b308d2ef805ee5d1679bad831339cbb1e55f
nhannp@vinanhatrang.vn <nhannp@vinanhatrang.vn>
2025-07-07 14:05:06 +0000

    Edited Form2; Deleted ColorPickerTemplate

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -2,17 +2,8 @@ from ._anvil_designer import Form2Template
 from anvil import *
 import anvil.server
 
+
 class Form2(Form2Template):
   def __init__(self, **properties):
     self.init_components(**properties)
-    self.color_picker_1.visible = False
-    self.color_picker_1.set_event_handler('x-color_changed', self._on_color_changed)
-
-  def button_show_click(self, **event_args):
-    self.color_picker_1.visible = not self.color_picker_1.visible
-
-  def _on_color_changed(self, **event_args):
-    color = event_args['color']
-    self.label_result.text = f"Bạn chọn: {color}"
-    self.label_result.foreground = color
-    self.color_picker_1.visible = False
+    
