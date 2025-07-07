fe33f2566832f9cb51399fa6ea341108a96909de
nhannp@vinanhatrang.vn <nhannp@vinanhatrang.vn>
2025-07-07 02:06:48 +0000

    Created Form_WheelGra; Deleted Form2

--- client_code/Form_WheelGra/__init__.py
+++ client_code/Form_WheelGra/__init__.py
@@ -1,9 +1,9 @@
-from ._anvil_designer import Form2Template
+from ._anvil_designer import Form_WheelGraTemplate
 from anvil import *
 import plotly.graph_objects as go
 import anvil.server
 
-class Form2(Form2Template):
+class Form_WheelGra(Form_WheelGraTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
